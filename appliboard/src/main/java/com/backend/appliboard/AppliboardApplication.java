2a8b662f479ed91125ef0bb272602da47072eef0
TomerP750 <tomer.jcb65@gmail.com>
2026-06-29 17:48:45 +0300

    backend init

---
+++ appliboard/src/main/java/com/backend/appliboard/AppliboardApplication.java
@@ -0,0 +1,13 @@
+package com.backend.appliboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliboardApplication.class, args);
+	}
+
+}
---
+++ appliboard/src/test/java/com/backend/appliboard/AppliboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.appliboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
