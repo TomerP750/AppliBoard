2a198de51b9c34ac16e5be30c1a2806c52b6d854
TomerP750 <tomer.jcb65@gmail.com>
2026-06-30 07:49:47 +0300

    update

--- frontend/src/layout/Routing.tsx
+++ frontend/src/layout/Routing.tsx
@@ -1,12 +1,13 @@
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../features/home/pages/Home";
 
 
 export function Routing() {
     return (
         <Routes>
             
+            <Route path="/" element={<Home />}/>
 
-            
 
         </Routes>
     )
