36c342cd39b9f91e4c91a95ea4c20b8ceb5c31b3
TomerP750 <tomer.jcb65@gmail.com>
2026-07-01 13:19:51 +0300

    service blueprint added

--- frontend/src/features/dashboard/applications/api/jobApplicationService.ts
+++ frontend/src/features/dashboard/applications/api/jobApplicationService.ts
@@ -1,7 +1,29 @@
+// dont add axios yet
 
+import type { CreateJobApplicationDto } from "../models/CreateJobApplicationDto";
+import { baseApiUrl } from "../../../../shared/util/baseApi";
 
 class JobApplicationService {
-    
+
+    async allJobApplications(page: number = 0, size: number = 10) {
+
+    }
+
+    async oneJobApplication(jobApplicationId: string) {
+
+    }
+
+    async createJobApplication(dto: CreateJobApplicationDto) {
+
+    }
+
+    async deleteJobApplication(jobApplicationId: string) {
+
+    }
+
+    async toggleJobApplicationFavorite(jobApplicationId: string) {
+
+    }
 }
 
 const jobApplicationService = new JobApplicationService();
