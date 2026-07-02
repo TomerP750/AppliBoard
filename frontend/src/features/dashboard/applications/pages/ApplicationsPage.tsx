b4381e29ce77e0fca3cbbcd4e8509450dfaad8b0
TomerP750 <tomer.jcb65@gmail.com>
2026-07-02 14:24:18 +0300

    bug fixed

--- frontend/src/features/dashboard/applications/pages/ApplicationsPage.tsx
+++ frontend/src/features/dashboard/applications/pages/ApplicationsPage.tsx
@@ -1,42 +1,31 @@
 import { useState } from "react";
 import { Briefcase, ListFilter, Plus } from "lucide-react";
-import { useSearchParams } from "react-router-dom";
 import { DashboardHeader } from "../../layout/DashboardHeader";
 import { Button } from "../../../../shared/ui/Button";
 import { SearchInput } from "../../../../shared/ui/SearchInput";
 import { ApplicationCard } from "../components/ApplicationCard";
-import { FilterMenu, type FilterValue } from "../components/FilterMenu";
+// import { FilterMenu, type FilterValue } from "../components/FilterMenu";
 import { CreateModal } from "../components/CreateModal";
 import { EmptyApplications } from "../components/EmptyApplications";
 import { useQuery } from "@tanstack/react-query";
 import jobApplicationService from "../api/jobApplicationService";
 
-
 export function ApplicationsPage() {
 
     const [addModalOpen, setAddModalOpen] = useState<boolean>(false);
     const [isFilterMenuOpen, setIsFilterMenuOpen] = useState(false);
-    const [searchParams, setSearchParams] = useSearchParams();
-    const filterParam = searchParams.get("filter");
-
+    // const [searchParams, setSearchParams] = useSearchParams();
+    
     const { data: applications } = useQuery({
         queryKey: ["applications"],
-        queryFn: () => jobApplicationService.allJobApplications(),
+        queryFn: () => jobApplicationService.allJobApplications(0, 10),
     });
 
-    const empty = !applications?.data || applications.data.length === 0;
-    
-    // const selectedFilter: FilterValue | null =
-    //     filterParam === "favorites" || filterParam === "in-progress" || filterParam === "recent"
-    //         ? filterParam
-    //         : null;
 
-    // const handleSelectFilter = (filter: FilterValue) => {
-    //     const nextParams = new URLSearchParams(searchParams);
-    //     nextParams.set("filter", filter);
-    //     setSearchParams(nextParams);
-    //     setIsFilterMenuOpen(false);
-    // };
+    const applicationsList = applications?.content ?? [];
+
+    const empty = applicationsList.length === 0;
+    
 
     return (
         <section className="min-h-screen bg-zinc-50 p-4 sm:p-6 dark:bg-dark-background">
@@ -83,7 +72,7 @@ export function ApplicationsPage() {
                     <EmptyApplications />
                 ) : (
                     <div className="mt-6 grid grid-cols-1 gap-4 md:grid-cols-3">
-                        {applications?.data.map((application) => (
+                        {applicationsList.map((application) => (
                             <ApplicationCard key={application.id} application={application} />
                         ))}
                     </div>
