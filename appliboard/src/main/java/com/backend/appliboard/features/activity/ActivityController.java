d6de8187e15bf0d71635bb132d5388bfc6aae489
TomerP750 <tomer.jcb65@gmail.com>
2026-07-06 18:55:52 +0300

    sort fix

--- appliboard/src/main/java/com/backend/appliboard/features/activity/ActivityController.java
+++ appliboard/src/main/java/com/backend/appliboard/features/activity/ActivityController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,7 @@ public class ActivityController {
     public Page<ActivityDto> allActivities(@AuthenticationPrincipal PrincipalUser user,
                                            @RequestParam(name = "page", defaultValue = "0") int page,
                                            @RequestParam(name = "size", defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
         UUID userId = user.getUserId();
         return activityService.allActivities(userId, pageable);
     }
--- frontend/src/features/dashboard/index/utils/getIconFromActivityType.tsx
+++ frontend/src/features/dashboard/index/utils/getIconFromActivityType.tsx
@@ -2,7 +2,6 @@ import { Plus, Edit, Trash } from "lucide-react";
 import { ActivityType } from "../models/ActivityDto";
 
 export const getIconFromActivityType = (type: ActivityType): React.ReactNode => {
-    console.log("TYPE RECEIVED:", type);
     switch (type) {
         case ActivityType.CREATED:
             return <Plus size={16} />;
