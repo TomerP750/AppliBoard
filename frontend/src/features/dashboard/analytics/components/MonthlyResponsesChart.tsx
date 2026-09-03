ca38cf54c55d21763fa2976751371ab34031ff96
TomerP750 <tomer.jcb65@gmail.com>
2026-09-03 11:16:51 +0300

    chart title fix

--- frontend/src/features/dashboard/analytics/components/MonthlyResponsesChart.tsx
+++ frontend/src/features/dashboard/analytics/components/MonthlyResponsesChart.tsx
@@ -17,9 +17,12 @@ export function MonthlyResponsesChart({ totalMonthlyResponses }: MonthlyResponse
     return (
         <figure className="w-full h-85 rounded-xl border border-black/10 dark:border-white/10 bg-white dark:bg-zinc-900 p-4">
             <ResponsiveContainer width="100%" height="100%">
-                <LineChart data={data} margin={{ top: 20, right: 8, left: 0 }}>
+                <LineChart data={data} margin={{ top: 32, right: 8, left: 12 }}>
                     <XAxis dataKey="day" tickMargin={10} interval={0} minTickGap={4} />
-                    <YAxis allowDecimals={false} label={{ value: "Responses", position: "top", offset: 10 }} />
+                    <YAxis
+                        allowDecimals={false}
+                        label={{ value: "Responses", position: "top", offset: 14, dx: 28 }}
+                    />
                     <CartesianGrid stroke="#ccc" />
                     <Tooltip
                         formatter={(value) => [value, "Responses"]}
